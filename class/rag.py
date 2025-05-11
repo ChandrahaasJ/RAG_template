ba9ecfbb6653068ae77e636a1eeaa5fd5aa78679
chandrahaasJ <chandrahaasjasti@gmail.com>
2025-05-12 00:43:31 +0530

    added pdf and web_parsing capabilities

--- class/rag.py
+++ class/rag.py
@@ -2,17 +2,50 @@ import faiss
 import os
 from pathlib import Path
 import json
+from trafilatura import fetch_url, extract
+import pymupdf4llm 
 
 class EmbRag:
+    cache=""
+    urls=[]
+    files=[]
     def __init__(self,docs_path,faiss_path):
         pth=os.path.join(faiss_path,"cache.json")
         self.pth_checker(pth)
         with open(pth,'r') as f:
             self.cache=json.load(f)
+        self.files=os.listdir(docs_path)
+
+        for i in self.files:
+            if i not in self.cache:
+                if(i.endswith('.docx')):
+                    print(True)
+                elif(i.endswith('.pdf')):
+                    md_text = pymupdf4llm.to_markdown(os.path.join(docs_path,i))
+                    print(md_text)
+                elif(i.endswith('.txt') and i.startswith('url')):
+                    with open(os.path.join(docs_path,i),'r') as f:
+                        links=f.read()
+                        self.urls=links.split(',')
+                    for j in self.urls:
+                        downloaded = fetch_url(j)
+                        result = extract(downloaded)
+                        if(result == None):
+                            print(f"could not access the url {j} because of authentication ")
+                        else:
+                            #chunker(result)
+                            print(result)
+                        
+                        
+
+                self.cache[i]="True"
+
+
+        with open(pth,'w') as f:
+            json.dump(self.cache,f,indent=4)
     
     def pth_checker(self,arge):
         file_path=Path(arge)
-        
         if(not file_path.exists()):
             file_path.parent.mkdir(parents=True, exist_ok=True)
             with open(file_path,'w') as f:
